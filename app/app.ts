568b9d99510cd0f6f84b249559266a1f03b96e59
sarangkulkarni99 <sarang.kulkarni99@gmail.com>
2024-08-27 16:23:51 +0530

    postgres connection setup

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { connectToPostgres } from "./connections/postgres.connection";
 
 export const startServer = async () => {
   try {
     const app = express();
 
     const { PORT } = process.env;
+
+    await connectToPostgres();
     app.listen(PORT, () => {
       console.log(`SERVER STARTED ON PORT ${PORT}`);
     });
